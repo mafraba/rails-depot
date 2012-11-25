586f150d72fdec283043f03da683af832355f323
Manuel Franco <mafraba@gmail.com>
2012-11-25 14:50:37 +0100

    Iteration D3 finished

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,6 +6,8 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class Cart < ActiveRecord::Base
-  has_many :line_items, dependent: :destroy
+class LineItem < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :cart
+  attr_accessible :cart_id, :product_id
 end
